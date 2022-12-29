f193f74741644216c7f9bdb65a5a8dcf0598bc3e
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2022-12-29 22:30:02 +0700

    Add a file Person.java

---
+++ 01/01-FamilyTree/Person.java
@@ -0,0 +1,13 @@
+package FamilyTree;
+
+public class Person {
+
+    private String fullName;
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
