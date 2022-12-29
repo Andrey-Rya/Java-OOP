16daff0e638c3a44b162a74ab0c52bdb6eec5225
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2022-12-29 22:28:11 +0700

    Add a file MyFirstApp.java

---
+++ 01/01-FamilyTree/MyFirstApp.java
@@ -0,0 +1,9 @@
+package FamilyTree;
+
+import java.nio.charset.Charset;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset().displayName());
+    }
+}
