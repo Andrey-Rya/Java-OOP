cb8c1def07fe462be82fdd0d827db95282b1b6a8
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2023-01-02 14:23:03 +0700

    Add a file Assignment2.java

---
+++ 02/02-1-Interface/Assignment2.java
@@ -0,0 +1,6 @@
+public class Assignment2 { // запускаем интерфейс программы
+
+    public static void main(String[] args) { // args содержит аргументы командной строки
+        TreeGUI treeGUI = new TreeGUI();
+    }
+}
