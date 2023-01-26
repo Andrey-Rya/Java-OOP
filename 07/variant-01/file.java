341234d39429200b90b392faa9e25657c3db3114
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2023-01-27 00:03:32 +0700

    Add a file file.java

---
+++ 07/variant-01/file.java
@@ -0,0 +1,22 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class file {
+
+    public ArrayList<String> readFile(String nameFile) throws IOException{
+        ArrayList<String> arrFile = new ArrayList<>();
+        try (FileReader fileRead = new FileReader(nameFile)){
+            Scanner scan = new Scanner(fileRead);
+            while(scan.hasNextLine()){
+                arrFile.add(scan.nextLine());
+            }
+            scan.close();
+        } catch(Exception e){
+            System.out.println("Ошибка парсинга" + e.toString());
+        }
+        return arrFile;
+    }
+}
